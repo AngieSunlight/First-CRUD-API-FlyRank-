08ca518b4d58e45a0560e769d1372c57758babe7
Angie Kimbowa <angie.kimbowa@student.manchester.ac.uk>
2026-08-07 19:36:08 +0100

    Stage 2: insert into database

--- main.py
+++ main.py
@@ -50,11 +50,17 @@ async def return_task(id: int):
 @app.post("/tasks", status_code = 201)
 async def add_task(task_data: Task):
     """Creates a new task with validation"""
-    if not task_data.title or not task_data.title.strip():
+    if not task_data.title or task_data.title.strip() == "":
         raise HTTPException(status_code=400, detail={"error": "Title is required"})
-    next_id = max([t["id"] for t in task_list], default=0) + 1
+    
+    conn = sqlite3.connect("tasks.db")
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO tasks (title, done) VALUES (?, ?)", (task_data.title,False,))
+    conn.commit()
+    next_id = cursor.lastrowid
+    conn.close()
     new_task = {"id": next_id, "title": task_data.title.strip(), "done": False}
-    task_list.append(new_task)
     return new_task
 
 @app.put("/tasks/{id}")
